1db12f43c8e4a345a12b101d067dc6c82738fe84
Tina Dominguez <tina@tinadominguez.com>
2015-11-20 21:39:56 -0500

    change paragraphs using jQuery
    three values include font-size, background-color, and color.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var paragraphs = $("p");
+    $("p").css ({
+        "font-size": "14", 
+        "color" : "black",
+        "background-color" : "#79AAB1",
+});
+});
+
